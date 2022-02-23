2d6c56406452713b02cd27098b73210b72a17ce0
AntonioStr <antonio.straziota10@gmail.com>
2022-02-23 03:20:46 +0100

    fine esercizio

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const openMenu = document.querySelector(".header-right > a");
+
+openMenu.addEventListener("click", function(){  
+    document.querySelector(".hamburger-menu").classList.add("active")
+});
+
+const closeMenu = document.querySelector(".close");
+
+closeMenu.addEventListener("click", function(){
+    document.querySelector(".hamburger-menu").classList.remove("active")
+});
